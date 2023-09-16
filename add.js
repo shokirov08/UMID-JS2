4027187d31a888edbec1ac4fe5a55a93fcf032ea
Umid <32@.ru>
2023-09-16 19:17:25 +0500

    dz2

---
+++ add.js
@@ -0,0 +1,111 @@
+// задание 1
+/* var A = +prompt('Введите ваш возраст в цифрах');
+if (A == 0 || A <= 18) {
+  alert('Вы еще молоды, Вам нужно учиться.');
+} 
+else if (A == 18  || A <= 50 ) {
+  alert('Вам нужно работать');
+} 
+else if (A == 50  || A <= 59 ) {
+  alert('Вам скоро на пенсию');
+} 
+else if (A == 59  || A <= 100 ) {
+  alert('Вы пенсионер');
+}else {
+  alert('Что-то пошло не так');
+} */ 
+
+
+// задание 2
+/* var B = +prompt('Введите время в цифрах:');
+
+switch(B) {
+  case 0: 
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  alert(B + ' часа ночи');
+  break
+  
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  alert(B + ' часов утра');
+  break
+  
+  case 11:
+  case 12:
+  alert(B + ' часа дня');
+  break
+  
+  case 13:
+  alert(1 + ' час дня');
+  break
+  
+  case 14:
+  alert(2 + ' часа дня');
+  break
+  
+  case 15:
+  alert(3 + ' часа дня');
+  break
+  
+  case 16:
+  alert(4 + ' часа дня');
+  break
+  
+  case 17:
+  alert(5 + ' часа дня');
+  break
+  
+  case 18:
+  alert(6 + ' часов вечера');
+  break
+  
+  case 19:
+  alert(7 + ' часов вечера');
+  break
+  
+  case 20:
+  alert(8 + ' часов вечера');
+  break
+  
+  case 21:
+  alert(9 + ' часов вечера');
+  break
+  
+  case 22:
+  alert(10 + ' часов вечера');
+  break
+  
+  case 23:
+  alert(11 + ' часов вечера');
+  break
+  
+  case 24:
+  alert(12 + ' часов вечера');
+  break
+  default:
+  alert( "Нет такого времени" );
+} */
+
+
+// задание 3
+
+/* var a = +prompt('Введите первое число');
+var b = +prompt('Введите второе число');
+var c = +prompt('Введите третье число');
+
+if (a<b && a>c || a>b && a<c) {
+  alert('Среднее число: ' + a)
+}else if (b<a && b>c || b>a && b<c) {
+  alert('Среднее число: ' + b)
+}else if (c<a && c>b || c>a && c<b) {
+  alert('Среднее число: ' + с)
+}else{
+  console.log('Что-то пошло не так');
+} */
